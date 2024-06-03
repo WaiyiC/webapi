276e64288c64eba6cf455095d9aa58087220c9fb
WaiyiC <106735570+WaiyiC@users.noreply.github.com>
2024-06-03 09:52:13 +0000

    Initial commit

---
+++ config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  host: "silly.db.elephantsql.com",
+  port: 5432,
+  user: "ekvbgjpl",
+  password: 'WzQwRrIf7vvalEfiebBAiv3wny7s0h9g',
+  database: "ekvbgjpl",
+  connection_limit: 100
+}
